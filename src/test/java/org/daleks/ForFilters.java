428a945d291b5596f6a4b8d1875c11e046c04c45
Iren Attano <79834812+dalekskaro@users.noreply.github.com>
2022-02-16 12:12:37 +0700

    .

---
+++ src/test/java/org/daleks/ForFilters.java
@@ -0,0 +1,10 @@
+package org.daleks;
+
+public class ForFilters {
+//"https://www.wildberries.ru/catalog/krasota/makiyazh"
+//"https://www.wildberries.ru/catalog/tovary-dlya-zhivotnyh/dlya-koshek"
+    //"https://www.wildberries.ru/catalog/sport/vidy-sporta/yoga/aksessuary-i-inventar"
+
+
+
+}
